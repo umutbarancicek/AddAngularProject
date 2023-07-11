f3ce2ea43d32152c97faffb62f8e7478569ed88f
Umut Baran Çiçek <20160808025@ogr.akdeniz.edu.tr>
2023-07-11 12:44:00 +0300

    AddAngularProject

---
+++ SuperHero.UI/src/app/services/super-hero.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { SuperHero } from '../models/super-hero';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/internal/Observable';
+import { environment } from 'src/environments/environment';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SuperHeroService {
+  private url = "SuperHero";
+
+  constructor(private http : HttpClient) { }
+
+  public getSuperHeroes() : Observable<SuperHero[]> {
+    return this.http.get<SuperHero[]>(`${environment.apiUrl}/${this.url}`);
+  }
+
+  public updateHero(hero:SuperHero) : Observable<SuperHero[]> {
+    return this.http.put<SuperHero[]>(`${environment.apiUrl}/${this.url}`,hero);
+  }
+
+  public createHero(hero:SuperHero) : Observable<SuperHero[]> {
+    return this.http.post<SuperHero[]>(`${environment.apiUrl}/${this.url}`,hero);
+  }
+
+  public deleteHero(hero:SuperHero) : Observable<SuperHero[]> {
+    return this.http.delete<SuperHero[]>(`${environment.apiUrl}/${this.url}/$(hero.id)`);
+  }
+}
